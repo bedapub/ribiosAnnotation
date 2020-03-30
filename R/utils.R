79c3ffc1c14a48c2283757150d75229e1e0ba317
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-03-30 11:36:52 +0200

    use secrets.json

--- R/utils.R
+++ R/utils.R
@@ -2,32 +2,41 @@
 #' @importFrom ribiosUtils matchColumn putColsFirst
 
 ##--------------------##
+## crendentials
+##--------------------##
+parseSecrets <- function(
+    path=file.path("/pstore/apps/bioinfo",
+                   "ribios/secrets",
+                   "ribiosAnnotation-secrets.json")) {
+  if (!file.exists(path)) {
+    warning("Can't find secret file: '", path, "'.")
+    path <- system.file("secrets/secrets-template.json", 
+                        package="ribiosAnnotation")
+  
+  return(jsonlite::read_json(path))
+}
+
+loadSecrets <- function(path=file.path("/pstore/apps/bioinfo",
+                   "ribios/secrets",
+                   "ribiosAnnotation-secrets.json")) {
+    credentials <- parseSecrets(path)
+    assign(ORACLE.BIA.USER, credentials$bia$username, pos=sys.frame())
+    assign(ORACLE.BIA.PWD, credentials$bia$password, pos=sys.frame())
+    assign(ORACLE.BIA2.USER, credentials$bia2$username, pos=sys.frame())
+    assign(ORACLE.BIA2.PWD, credentials$bia2$password, pos=sys.frame())
+    assign(ORACLE.BIARO.USER, credentials$biread$username, pos=sys.frame())
+    assign(ORACLE.BIARO.PWD, credentials$biread$password, pos=sys.frame())
+    assign(ORACLE.BIN.USER, credentials$bin$username, pos=sys.frame())
+    assign(ORACLE.BIN.PWD, credentials$bin$password, pos=sys.frame())
+}
+
+##--------------------##
 ## constants
 ##--------------------##
-## bi@bia
-ORACLE.BIA.USER <- "bi"
-ORACLE.BIA.PWD <- "bi"
-## bi2@bia
-ORACLE.BIA2.USER <- "bi2"
-ORACLE.BIA2.PWD <- "wolf"
-## biread@bia
-ORACLE.BIARO.USER <- "biread"
-ORACLE.BIARO.PWD <- "biread"
-## red
-ORACLE.RED.USER <- "red"
-ORACLE.RED.PWD <- "red"
-## genome
-## Note that in 2018/09, bin was migrated to bia. The same user/password for bin remain valid for bia.
-ORACLE.BIN.USER <- "genome"
-ORACLE.BIN.PWD <- "genome"
-
-ORACLE.BASE <- "/opt/oracle"
-ORACLE.HOME <- "/opt/oracle/client/10/run_1"
 ## ORACLE.LIB <- ":/opt/oracle/client/10/run_1/lib"
 ## maximum vector length in the IN syntax
 #' @export ORACLE.IN.NMAX
 ORACLE.IN.NMAX <- 1000L
-
 
 ## function to test whether Oracle is available
 #' @export hasOracle
@@ -37,30 +46,19 @@ hasOracle <- function() {
 
 ## onload / onAttach
 ## .onLoad <- function(libname, pkgname) {}
-
 .onAttach <- function(libname, pkgname) {
   if(hasOracle()) {
-    ##    require(ROracle)
     if(!"package:ROracle" %in% search())
       attachNamespace("ROracle")
-    obase <- Sys.getenv("ORACLE_BASE")
-    ohome <- Sys.getenv("ORACLE_HOME")
-    if(identical(obase, "") || identical(obase, ".") || !"client" %in% dir(obase)) {
-      Sys.setenv("ORACLE_BASE"=ORACLE.BASE)
-    }
-    if(identical(ohome, "") || identical(ohome, ".") || !"bia" %in% dir(ohome)) {
-      Sys.setenv("ORACLE_HOME"=ORACLE.HOME)
-    }
     assign("ORA", ROracle::Oracle(), pos=sys.frame())
-  } else {
-    ## require(RJDBC)
-  }
+  } 
+  loadSecrets()
 }
 
 ## automatically establish a connection, depending on whether Oracle client is installed
 #' @export ribiosCon
 ## TODO: 1g memorz at least is hard-coded. How to infer it?
-ribiosCon <- function(db="bia", user="biread", password="biread", forceJDBC=FALSE) {
+ribiosCon <- function(db="bia", user=ORACLE.BIARO.USER, password=ORACLE.BIARO.PWD, forceJDBC=FALSE) {
   if(hasOracle() & !forceJDBC) {
     con <- dbConnect(ORA, user = user, password = password, db = db)
   } else {
@@ -82,7 +80,6 @@ newconBIA <- function() ribiosCon(db="bia", user=ORACLE.BIA.USER, password=ORACL
 newconBIA2 <- function() ribiosCon(db="bia", user=ORACLE.BIA2.USER, password=ORACLE.BIA2.PWD)
 newconBIARO <- function() ribiosCon(db="bia", user=ORACLE.BIARO.USER, password=ORACLE.BIARO.PWD)
 newconBIN <- function() ribiosCon(db="bia", user=ORACLE.BIN.USER, password=ORACLE.BIN.PWD)
-newconRED <- function() ribiosCon(db="bia", user=ORACLE.RED.USER, password=ORACLE.RED.PWD)
 
 ##----------------------------------------##
 ## deprecated functions
