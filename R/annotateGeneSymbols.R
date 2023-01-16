a32843b36f34af59b65a8140bafa2f427d2cca30
Jitao David Zhang <jitao_david.zhang@roche.com>
2023-01-16 16:27:31 +0100

    add a decoy of annotateGeneSymbols

---
+++ R/annotateGeneSymbols.R
@@ -0,0 +1,12 @@
+#' Annotate GeneSymbols
+#' @param ids Character strings, gene symbols
+#' @param orthologue Logical, whether orthologues are to be returned
+#' @param multiOrth Logical, only valid when orthologue is set to TRUE, whether
+#' multiple orthologues are returned
+#' @param organism Character string, organism
+#' Now the function is not functional
+#' @export
+annotateGeneSymbols <- function(ids, orthologue=FALSE, multiOrth=FALSE, 
+				organism=c("human", "mouse", "rat", "any")) {
+	return(NULL)
+}
